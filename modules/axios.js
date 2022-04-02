ba91a4014b29f2109b75da44c1eb944e33ef0859
Robert Reinhard <robert.reinhard@bukwild.com>
2022-04-02 07:23:34 -0700

    Add axios module

---
+++ modules/axios.js
@@ -0,0 +1,18 @@
+export default function() {
+
+	// Add the Axios Nuxt module
+	this.requireModule('@nuxtjs/axios')
+
+	// Set default config
+	this.options.axios = {
+
+		// Prevent axios from using localhost:3000 when SSGed
+		baseURL: process.env.URL,
+
+		// Don't trigger loader UI
+		progress: false,
+
+		// Support overriding from project
+		...this.options.axios
+	}
+}
