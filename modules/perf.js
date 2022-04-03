68c474b39155314ddcc734b983cc257cc3ae0bed
Robert Reinhard <robert.reinhard@bukwild.com>
2022-04-03 09:23:48 -0700

    Don’t use defaultsDeep on built in options

--- modules/perf.js
+++ modules/perf.js
@@ -2,24 +2,17 @@
  * Tweaks intended for perf
  */
 import { isDev } from '../helpers/build'
-import defaultsDeep from 'lodash/defaultsDeep'
 export default function() {
-	defaultsDeep(this.options, {
 
-		// Extract CSS into seperate files when statically generating to reduce JS
-		// cost of injecting styles and to make them more cacheable
-		build: {
-			extractCSS: !isDev
-		},
+	// Extract CSS into seperate files when statically generating to reduce JS
+	// cost of injecting styles and to make them more cacheable
+	this.options.build.extractCSS = !isDev
 
-		// Remove preloading JS and CSS to improve PageSpeed scores. This pains me
-		// to do because it seems so reasonable to have enabled but this does move
-		// the needle on scores.
-		render: {
-			resourceHints: false
-		},
+	// Remove preloading JS and CSS to improve PageSpeed scores. This pains me
+	// to do because it seems so reasonable to have enabled but this does move
+	// the needle on scores.
+	this.options.render.resourceHints = false
 
-		// Make seperate lighter build for modern browsers
-		modern: isDev ? false : 'client',
-	})
-};
+	// Make seperate lighter build for modern browsers
+	this.options.modern = isDev ? false : 'client'
+}
