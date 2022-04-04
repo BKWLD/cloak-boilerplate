9b4b2a827260b1c5d3ab14e02287b03fd3c83531
Robert Reinhard <robert.reinhard@bukwild.com>
2022-04-03 20:55:01 -0700

    Add routing module
    Closes https://github.com/BKWLD/cloak/issues/49

---
+++ modules/routing.js
@@ -0,0 +1,51 @@
+/*
+ * Add various helpers related to routing
+ */
+import defaultsDeep from 'lodash/defaultsDeep'
+import { join } from 'path'
+import { existsSync } from 'fs'
+import { sortRoutes } from '@nuxt/utils'
+export default function() {
+
+	// Register the vue-routing-anchor-parser module
+	this.requireModule('vue-routing-anchor-parser/nuxt/module')
+
+	// Make the tower slug optional, so the root route will match. This also
+	// adds support for slashes within Tower URIs.
+	if (existsSync(join(this.options.srcDir, 'pages/_tower.vue'))) {
+		this.extendRoutes(routes => {
+			routes.find(route => route.name == 'tower').path = '/:tower*'
+			sortRoutes(routes)
+		})
+	}
+
+	// Add default anchor parser rules
+	defaultsDeep(this.options, { anchorParser: {
+		addBlankToExternal: true,
+		internalUrls: [
+
+			// Local dev
+			/^https?:\/\/localhost:\d+/,
+
+			// Use current URL from Netify
+			(function() {
+				if (!process.env.URL) return
+				const url = process.env.URL.replace(/[\/\-\.]/g, '\\$&')
+				return new RegExp(`^${url}`)
+			})(),
+
+			// Use Netlify subdomain URL
+			(function() {
+				if (!process.env.URL) return
+				const name = process.env.URL.replace(/[\-]/g, '\\$&')
+				return new RegExp(`^https?:\/\/${name}\.netlify\.app`)
+			})()
+		],
+		externalPaths: [
+
+			// Don't client-side navigate to Netlify functions
+			/^\/\.netlify/,
+		]
+	}})
+
+}
