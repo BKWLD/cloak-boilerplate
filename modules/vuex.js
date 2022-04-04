94ada96e197036372dd37cb4d33b75d051d1e888
Robert Reinhard <robert.reinhard@bukwild.com>
2022-04-04 09:12:36 -0700

    Don’t load nuxt-spa-store-init if no store/index

--- modules/vuex.js
+++ modules/vuex.js
@@ -1,9 +1,14 @@
 /*
  * Configuration related to setting up vuex
  */
+import { join } from 'path'
+import { existsSync } from 'fs'
 export default function() {
 
-	// Run storeInit when on spa mode
-	this.requireModule('nuxt-spa-store-init')
+	// Run storeInit when on spa mode if the project has an index module
+	if (existsSync(join(this.options.srcDir, 'store/index.js')) ||
+		existsSync(join(this.options.srcDir, 'store/index.coffee'))) {
+		this.requireModule('nuxt-spa-store-init')
+	}
 
 }
