0494ac0b3cee18f34edd62b880cb28944fe66a7a
Robert Reinhard <robert.reinhard@bukwild.com>
2022-04-16 08:20:08 -0700

    Renaming helpers
    Trying to solve intermitent “Cannot access 'helper' before initialization” error

--- plugins/inject-helper.js
+++ plugins/inject-helper.js
@@ -1,9 +1,9 @@
 /*
  * Plugin template that imports a helper and injects all methods.
  */
-import * as helper from '<%= options.helperFile %>'
+import * as helpers from '<%= options.helperFile %>'
 export default function(context, inject) {
-	for (const [name, helper] of Object.entries(helper)) {
+	for (const [name, helper] of Object.entries(helpers)) {
 
 		// We bind the context to the method so any context method can be invoked
 		// using `this`.
