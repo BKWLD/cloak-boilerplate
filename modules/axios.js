d951b88e70e0d9f26f6b86aab2e3ceafa97455aa
Robert Reinhard <robert.reinhard@bukwild.com>
2022-09-15 07:32:41 -0700

    Refactor custom retry condition
    Moving code into the Nuxt module and importing more from axios-retry

--- modules/axios.js
+++ modules/axios.js
@@ -3,7 +3,7 @@
  */
 import defaultsDeep from 'lodash/defaultsDeep'
 import { requireLate } from '@cloak-app/utils'
-import { retryCondition } from '../helpers/axios'
+import { isNetworkError } from 'axios-retry'
 export default function() {
 
 	// Set default config
@@ -22,3 +22,30 @@ export default function() {
 	// Add the Axios Nuxt module late so it can be used in plugins
 	requireLate(this, '@nuxtjs/axios')
 }
+
+// Retry GQL requests that don't have a mutation
+function retryCondition(error) {
+	return isNetworkError(error) ||
+		isIdempotentRequestError(error) ||
+		isGQLQuery(error)
+}
+
+// Allow POST requests that look like GQL queries (aka, non-mutations)
+function isGQLQuery(error) {
+
+	// Only act on POSTs
+	if (error.config.method != 'post') return
+
+	// Test whether the request body is non-empty
+	const data = error.config?.data
+	if (!data) return
+
+	// ... and contains JSON
+	let body
+	try { body = JSON.parse(data) }
+	catch (e) { return }
+
+	// Ensure the request is not a mutation
+	return typeof body.query === 'string' && !body.query.includes('mutation')
+}
+
